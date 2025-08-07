3918828492ec91570ed8f294e25de678ef606bfa
ruben <ruben@302.nl>
2025-08-07 21:20:39 +0200

    refactor(env): make DATABASE_URL a required field in validation and remove redundant data source configuration

--- backend/src/settings/database/data-source.ts
+++ backend/src/settings/database/data-source.ts
@@ -9,17 +9,6 @@ const DATABASE_URL = process.env.DATABASE_URL;
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: DATABASE_URL,
-  host: DATABASE_URL ? undefined : (process.env.POSTGRES_HOST ?? 'localhost'),
-  port: DATABASE_URL ? undefined : Number(process.env.POSTGRES_PORT ?? '5432'),
-  username: DATABASE_URL
-    ? undefined
-    : (process.env.POSTGRES_USER ?? 'postgres'),
-  password: DATABASE_URL
-    ? undefined
-    : (process.env.POSTGRES_PASSWORD ?? 'postgres'),
-  database: DATABASE_URL
-    ? undefined
-    : (process.env.POSTGRES_DB ?? 'nesttracker'),
   entities: [__dirname + '/../../../**/*.entity.{ts,js}'],
   synchronize:
     environmentFromString(process.env.NODE_ENV ?? 'development') !==
